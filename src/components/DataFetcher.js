8763575aa2b6c63f41baad670324d8e41def4054
misterKumar <120304503+misterKumar@users.noreply.github.com>
2023-09-05 20:15:18 +0530

    check 2

--- src/components/DataFetcher.js
+++ src/components/DataFetcher.js
@@ -3,11 +3,17 @@ import React, { useState, useEffect } from 'react';
 function DataFetcher() {
   const [data, setData] = useState(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     // Fetch data from the API
     fetch('https://dummyjson.com/products')
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
       .then((jsonData) => {
         // Update the state with the fetched data
         setData(jsonData);
@@ -15,6 +21,7 @@ function DataFetcher() {
       })
       .catch((error) => {
         console.error('Error fetching data:', error);
+        setError(error);
         setIsLoading(false); // Set loading to false in case of an error
       });
   }, []);
@@ -24,12 +31,15 @@ function DataFetcher() {
       <h1>Data Fetched from API</h1>
       {isLoading ? (
         <p>Loading...</p>
-      ) : (
+      ) : error ? (
+        <p>Error: {error.message}</p>
+      ) : data ? (
         <pre>{JSON.stringify(data, null, 2)}</pre>
+      ) : (
+        <p>No data found</p>
       )}
     </div>
   );
 }
 
 export default DataFetcher;
-
